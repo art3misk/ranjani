2ebab55bca572dad1a323f653b65d90c32c15c16
art3misk <art3miskou@gmail.com>
2025-05-09 17:45:12 -0400

    WOHOOOO

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+from google import genai
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        output = query_document(user_input)
+        return render_template('index.html', output=output, input=user_input)
+    return render_template('index.html')
+
+def query_document(question):
+    response = chat.send_message(f"Someone is on my personal website that I made, and this is the question they're asking: {question}. Make the answer short and succint, and highlight points from my resume. You can also extrapolate a little and add some personality to your response. But also make sure it's proffesional sounding, as if a collegue or employer or student is asking it. If they ask a personal question then make up a funny response (it doesnt have to be true) . Here is my resume: {content}")
+    return response.text
+
+if __name__ == '__main__':
+    with open('test.txt', 'r') as file:
+        content = file.read()
+    
+    gemini_key = 'AIzaSyCUMAaofbAemqtyPEO_v7NkJlwyfIRfWSo'
+
+    client = genai.Client(api_key=gemini_key)
+    chat = client.chats.create(model="gemini-2.0-flash")
+    app.run(debug=True)
